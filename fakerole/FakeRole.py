0583907ec3d2ef653af5ce789d91c51d45860920
Elise <27158829+elise32@users.noreply.github.com>
2024-04-19 11:57:20 -0400

    Add a bit to FakeRole

--- fakerole/FakeRole.py
+++ fakerole/FakeRole.py
@@ -25,12 +25,17 @@ def is_valid_member(self, member:str) -> bool:
     return bool(re.match(r'^[0-9]{17,20}$', member))
 
 class FakeRole:
+    """
+    These maxima are not chosen arbitrarily. Discord's maximum bot message size is 2000 characters.
+    The markdown to hide pings takes 1000 characters. Ping markdown will be a max of 21 characters
+    until 2090. 2000-1000-(21*40) = 160 characters, enough for 30 characters, 6 emoji, and change
+    """
     NAME_MAX_LENGTH = 30
     MEMBERS_MAX_SIZE = 40
 
-    def __init__(self, id: str, name: str, members: set[str]):
+    def __init__(self, id: str, name: str, members: set[str], create_in_storage=True):
         if not id:
-            raise IDSizeError("ID for FakeRole cannot be none")
+            raise IDSizeError("ID for FakeRole cannot be empty")
         self._id: str = id
 
         self.name: str = name
@@ -39,6 +44,9 @@ class FakeRole:
             raise MemberSizeError(f"Member size for FakeRole {self.id} {self.name} was incorrect, \
                                   was {len(members)}")
         self._members: set[str] = members
+
+        if create_in_storage:
+            storage_helper.create_group(self.id, self.name, self.members)
     
     @property
     def id(self):
@@ -49,18 +57,19 @@ class FakeRole:
         return self._name
     
     @name.setter
-    def name(self, name:str) -> str:
+    def name(self, name:str):
         if not name or len(name) > self.NAME_MAX_LENGTH:
             raise NameSizeError(f"Name for FakeRole {self.id} {self.name} was incorrect, tried to \
                                 set it to {name}, must be <= {self.NAME_MAX_LENGTH}")
+        if name == self._name:
+            return
+
         self._name = name
+        storage_helper.change_group_name(self.id, name)
 
     @property
     def members(self) -> frozenset:
-        """
-        Do not directly modify members. Instead use FakeRole.add and FakeRole.remove
-        """
-        return self._members
+        return frozenset(self._members)
     
     def add(self, member) -> bool:
         """
@@ -91,10 +100,20 @@ class FakeRole:
         self._members.remove(member)
         FakeRoleStorageHelper.remove_member(self.id, member)
         if not self.members:
-            storage_helper.delete_group(self.id)
+            self.delete()
         return True
+    
+    def delete(self) -> bool:
+        """
+        Returns whether the FakeRole was successfully deleted
+        """
+        return storage_helper.delete_group(self.id)
 
-def get_all_FakeRoles() -> set[FakeRole]:
-    return storage_helper.get_all()
+def fetch_all_FakeRoles() -> set[FakeRole]:
+    return {
+            FakeRole(id, name, members, create_in_storage=False)
+            for id, name, members
+            in storage_helper.get_all()
+            }
 
 storage_helper = FakeRoleStorageHelper()
--- fakerole/FakeRoleStorageHelper.py
+++ fakerole/FakeRoleStorageHelper.py
@@ -8,6 +8,12 @@ class FakeRoleStorageHelper:
     def remove_member(self, id:str, member: str) -> bool:
         return False
     
+    def change_group_name(self, id:str, name:str) -> bool:
+        return False
+    
+    def create_group(self, id:str, name:str, members: set[str]) -> bool:
+        return False
+    
     def delete_group(self, id:str) -> bool:
         return False
     
